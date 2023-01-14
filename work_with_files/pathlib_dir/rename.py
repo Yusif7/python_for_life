22d48e7db4957d7cec551a2e29de92a57699e6dc
Yusif1989 <o.yusif@hotmail.com>
2023-01-14 23:30:04 +0400

    pathlib

---
+++ work_with_files/pathlib_dir/rename.py
@@ -0,0 +1,16 @@
+# Rename files in files directory
+from pathlib import Path
+
+# Define the root folder
+root_dir = Path('files')
+# Create a generator files
+file_paths = root_dir.iterdir()
+# List of files -> list(file_paths
+
+for path in file_paths:
+    new_filename = 'new-' + path.stem + path.suffix
+    # new_filepath = Path(new_filename) -> when we use whis format we rename names of files and move them inside of
+    # files directory
+    # When we use this way we change names of files name inside of files directory
+    new_filepath = path.with_name(new_filename)
+    path.rename(new_filepath)
