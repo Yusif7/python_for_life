afadd96553a4eb0e89633bc08e694ef89e230241
Yusif1989 <o.yusif@hotmail.com>
2023-01-31 01:17:37 +0400

    python tools

--- python_tools/streamlit/main.py
+++ python_tools/streamlit/main.py
@@ -1,8 +1,16 @@
 import streamlit as st
+import pandas
 
+data = {
+    'Series_1': [1, 3, 4, 5, 7],
+    'Series_2': [10, 30, 40, 100, 250]
+}
+
+df = pandas.DataFrame(data)
 st.title("Our fisrt App")
 st.subheader("Instroduction to Streamlit")
 st.write("""
     This is our first Web Application!
     Enjoy it!
 """)
+st.write(df)
